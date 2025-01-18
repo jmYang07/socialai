0983f1897b30557f6f2fcaeb80e079a2d94b0a6a
Jiemin Yang <yangjiemin07@gmail.com>
2025-01-18 03:09:41 +0000

    Initial commit

---
+++ constants/constants.go
@@ -0,0 +1,10 @@
+package constants
+
+const(
+	USER_INDEX = "user"
+	POST_INDEX = "post"
+	ES_URL = "http://10.128.0.2:9200"
+	ES_USERNAME = "jiemin"
+	ES_PASSWORD = "welcome"
+	GCS_BUCKET = "socialai-bucket2"
+)
